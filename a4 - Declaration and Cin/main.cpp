606033de402027560fe4575a76d20489a6c11b6d
estehangat <96716034+estehangat@users.noreply.github.com>
2023-08-14 20:46:48 +0700

    deklarasi dan cin

---
+++ a4 - Declaration and Cin/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    a = 21;
+    cout << "Angka pertama adalah ";
+    cout << a << endl;
+    
+    int b;
+    cout << "Masukkan angka kedua : ";
+    cin >> b;
+    cout << "Angka kedua adalah ";
+    cout << b << endl;
+
+    cin.get();
+    return 0;
+}
