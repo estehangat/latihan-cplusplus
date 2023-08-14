a8c92d9a459ffb4365543524f6322fc75bd484f3
estehangat <96716034+estehangat@users.noreply.github.com>
2023-08-14 20:31:08 +0700

    variabel

---
+++ a3 - Variable/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int a = 21;
+    int b = 9;
+
+    std::cout << "Hasilnya adalah " << a+b << std::endl;
+    std::cout << "Press any key and enter to end" << std::endl;
+    std::cin.get();
+    return 0; // Output : Hasilnya adalah 30
+}
