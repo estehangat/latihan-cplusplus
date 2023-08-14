f5165a8e68acd6adfbfd2654dfb1d726901bccf7
estehangat <96716034+estehangat@users.noreply.github.com>
2023-08-14 19:53:48 +0700

    latihan perdana

---
+++ a1 - Compiling, Linking/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/*
+    Testing Line
+*/
+
+int plus(int a, int b);
+
+int main()
+{
+    std::cout << "Hasilnya adalah " << plus(7,3) << std::endl; // Output : Hasilnya adalah 21
+    return 0;
+}
