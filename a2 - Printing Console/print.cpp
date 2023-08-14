1f04a51f0437ccbf96d13211621dee9778ac8e48
estehangat <96716034+estehangat@users.noreply.github.com>
2023-08-14 20:19:11 +0700

    printing console

---
+++ a2 - Printing Console/print.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Welcome\n";
+    std::cout << "To\n";
+    std::cout << "Kuliah" << std::endl;
+    std::cin.get();
+    return 0; // Output : Welcome To Kuliah
+}
